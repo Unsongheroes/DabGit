7e5400d8c7605166a897bf0b61d02d86a4c7f890
Malthe <malthedj@hotmail.com>
2018-03-02 15:30:13 +0100

    minor changes to application project

--- HandIn2.1.Application/Program.cs
+++ HandIn2.1.Application/Program.cs
@@ -10,6 +10,16 @@ namespace HandIn2._1.Application
     {
         static void Main(string[] args)
         {
+            ByPostnummer aarhus = new ByPostnummer(8000, "Aarhus", "Denmark");
+            Adresse ad1 = new Adresse("Silkeborgvej", 22, aarhus, "Hjem");
+
+
+
+            TelefonNummer JensArbejd = new TelefonNummer(12345678,"Arbejd", "Telia");
+            Person person = new Person(11111900, "Jens", "Poulsen", "Kollega", JensArbejd, ad1,"jens@gmail.com", "Peter");
+
+
+
         }
     }
 }

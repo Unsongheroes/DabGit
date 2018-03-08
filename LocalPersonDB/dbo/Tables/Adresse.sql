9e223e52d7fdaf48f40566224e84025e8b988a87
Kristoffer Stampe Villadsen <Villadsen67@hotmail.com>
2018-03-08 14:54:31 +0100

    Added Database script

---
+++ LocalPersonDB/dbo/Tables/Adresse.sql
@@ -0,0 +1,19 @@
+﻿--
+-- Create Table    : 'Adresse'   
+-- Adresse         :  
+-- Vejnavn         :  
+-- Husnummer       :  
+-- By_PostNummer   : Foreign Key 
+-- PostNummer      :  (references By_Postnummer.PostNummer)
+--
+CREATE TABLE Adresse (
+    Adresse        NVARCHAR NOT NULL,
+    Vejnavn        NVARCHAR NOT NULL,
+    Husnummer      NVARCHAR NOT NULL,
+    By_PostNummer  NVARCHAR NOT NULL,
+    PostNummer     BIGINT NOT NULL,
+CONSTRAINT pk_Adresse PRIMARY KEY CLUSTERED (Adresse),
+CONSTRAINT fk_Adresse FOREIGN KEY (PostNummer)
+    REFERENCES By_Postnummer (PostNummer)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
---
+++ LocalPersonDB/dbo/Tables/By_Postnummer.sql
@@ -0,0 +1,11 @@
+﻿--
+-- Create Table    : 'By_Postnummer'   
+-- PostNummer      :  
+-- Bynavn          :  
+-- Land            :  
+--
+CREATE TABLE By_Postnummer (
+    PostNummer     BIGINT NOT NULL,
+    Bynavn         NVARCHAR NOT NULL,
+    Land           NVARCHAR NOT NULL,
+CONSTRAINT pk_By_Postnummer PRIMARY KEY CLUSTERED (PostNummer))

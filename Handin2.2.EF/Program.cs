61f3e65f5fa551f3f3df979f031184583313a123
Malthe <malthedj@hotmail.com>
2018-03-08 14:47:48 +0100

    added project for use with entity framework

---
+++ Handin2.2.EF/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handin2._2.EF
+{
+    class Program
+    {
+        
+    }
+}

781f2af8c76afb9b0e2423b94ffc2dcc6d1363b7
Kristoffer Stampe Villadsen <Villadsen67@hotmail.com>
2018-03-31 16:58:19 +0200

    start på DDB handin2.2

---
+++ HandIn2_2_DDB/Personkatotek.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HandIn2_2_DDB
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Cpr { get; set; }
+        public string Fornavn { get; set; }
+        public string MellemNavn { get; set; }
+        public string EfterNavn { get; set; }
+        public string PersonType { get; set; }
+        public string Email { get; set; }
+        public TelefonNummer[] TelefonBog { get; set; }
+        public PersonAdresse[] PersonAdresses { get; set; }
+
+        /*
+         * uvidst om der skal være en constructor - dette spørgsmål gælder for alle classerne i denne fil.
+         */
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Adresse
+    {
+        public int AdresseId { get; set; }
+        public string VejNavn { get; set; }
+        public int Husnummer { get; set; }
+        public PersonAdresse[] PersonAdresses { get; set; }
+        public ByPostNummer ByPostNummer { get; set; }
+    }
+
+    public class TelefonNummer
+    {
+        public int Telefonnummer { get; set; }
+        public string TelefonnummerType { get; set; }
+        public string TelefonSelskab { get; set; }
+        public int PersonCpr { get; set; }
+        public Person Person { get; set; }
+    }
+
+    public class ByPostNummer
+    {  
+        public int Postnummer { get; set; }
+        public string ByNavn { get; set; }
+        public string Land { get; set; }
+    }
+
+    public class PersonAdresse
+    {
+        public int MatchId { get; set; }
+        public string Type { get; set; }
+        public int PersonCpr { get; set; }
+        public Person Person { get; set; }
+        public int AdresseId { get; set; }
+        public Adresse Adresse { get; set; }
+    }
+
+}
---
+++ HandIn2_2_DDB/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace HandIn2_2_DDB
+{
+    class Program
+    {
+
+        private const string EndpointUrl = "https://localhost:8081";
+
+        private const string PrimaryKey =
+            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        private DocumentClient _client;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                p.UniExDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+
+        private async Task UniExDemo()
+        {
+            this._client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+
+            await this._client.CreateDatabaseIfNotExistsAsync(new Database { Id = "PersonKatotekDB" });
+
+            await this._client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("PersonKatotekDB"), new DocumentCollection { Id = "PersonKatotek" });
+
+
+        }
+    }
+}
